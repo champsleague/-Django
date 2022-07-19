6c1c9b6a437c87a29eaecfdd2707fde4eb8941c2
youkeongjun <youkeongjun@naver.com>
2022-07-20 00:24:58 +0900

    view,urls 관계 수정

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('welcome')
+
+def create(request):
+    return HttpResponse('create')
+
+def read(request,id):
+    return HttpResponse('read'+id)
