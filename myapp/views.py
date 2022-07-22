b9c988814f3ebf77c7a9df6e67f5e7d5743a1b62
youkeongjun <youkeongjun@naver.com>
2022-07-22 23:14:43 +0900

    delete 기능 추가

--- myapp/views.py
+++ myapp/views.py
@@ -8,7 +8,7 @@ topics = [
     {'id':3, 'title':'model', 'body':'Model is...'},
     ]
 
-def HTMLTemplate(articleTag):
+def HTMLTemplate(articleTag,id=None):
     global topics
     ol = ''
     for topic in topics:
@@ -23,6 +23,12 @@ def HTMLTemplate(articleTag):
         {articleTag}
         <ul>
             <li><a href="/create">create</a></li>
+            <li>
+                <form action="/delete/" method="post">
+                    <input type="hidden" name="id" value={id}>
+                    <input type="submit" value="delete">
+                </form>
+            </li>
         <ul>
         </body>
         </html>
