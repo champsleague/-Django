4f87712f9ad89dfd1cb0ee8a3215708a530a701f
youkeongjun <youkeongjun@naver.com>
2023-02-03 07:33:01 +0900

    update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ window.initMap = function(){
     new google.maps.Map(document.getElementById("map"),{
         center: { lat: 37.1926035, lng: -123.8100657},
         zoom: 10
-    })
-}
+    });
+};
