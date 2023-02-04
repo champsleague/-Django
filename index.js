48f6f2c18d3c52ec894bf836713915cad4b42cad
youkeongjun <youkeongjun@naver.com>
2023-02-04 10:57:20 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 window.initMap = function(){
-    new google.maps.Map(document.getElementById("map"),{
+    const map = new google.maps.Map(document.getElementById("map"),{
         center: { lat: 37.1926035, lng: -123.8100657},
-        zoom: 10
+        zoom: 5
     });
+
+    new google.maps.Marker({
+        position: {lat:33.696049, lng:-117.8588907},
+        label: "PeopleSpace",
+        map: map
+    })
 };
