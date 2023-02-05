ce8119acba6ae8c1e75625514733e009b8318509
youkeongjun <youkeongjun@naver.com>
2023-02-05 12:08:59 +0900

    loc update

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ window.initMap = function(){
     const angry = [
         { label: "bacon", name: "Jack in the Box", lat: 33.7269096, lng: -118.0679232 },
         { label: "bacon", name: "Corner Bakery", lat: 33.69562, lng: -117.9233953 },
-        { label: "bacon", name: "Paragon", lat: 33.6939472, lng: -117.9395769 },
         { label: "bacon", name: "Yellow Basket Restaurant Santa Ana", lat: 33.7111182, lng: -117.8953854 },
         { label: "bacon", name: "Nate's Korner", lat: 33.6946383, lng: -117.901714 },
 
         { label: "peanuts", name: "Target", lat: 33.6939448, lng: -117.9395769 },
         { label: "peanuts", name: "Walmart Supercenter", lat: 33.7461456, lng: -118.0803732 },
-        { label: "peanuts", name: "peanuts", lat: 33.7461456, lng: -118.0803732 },
-        { label: "peanuts", name: "peanuts", lat: 33.7461456, lng: -118.0803732 },
-        // { label: "chocolate bar", name: "chocolate bar", lat: 33.7054491, lng: -117.9489364 },
-        // { label: "popcorn", name: "popcorn", lat: 33.705422, lng: -117.949221 },
+        { label: "peanuts", name: "Vons", lat: 33.7209513, lng: -117.9672333 },
+        { label: "peanuts", name: "Peanuts Pub & Deli", lat: 33.7123402, lng: -117.9663691 },
+        
+        { label: "chocolate bar", name: "ROYCE Chocolate", lat: 33.7067248, lng: -118.046865 },
+        { label: "chocolate bar", name: "Chocolate Bash", lat: 33.6502638, lng: -118.0069238 },
+
+        { label: "popcorn", name: "popcorn", lat: 33.705422, lng: -117.949221 },
         // { label: "cookie", name: "cookie", lat: 33.7053824, lng: -117.9495645 },
         // { label: "doughnut", name: "doughnut", lat: 33.7567961, lng: -117.9408495 },
         // { label: "rice crackers", name: "rice crackers", lat: 33.7567961, lng: -117.9408495 },
